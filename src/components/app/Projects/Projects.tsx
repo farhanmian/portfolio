5b4d848a8fa97e81d76cb88cee6834fb7b75ac55
farhanmian <farhanmian099@gmail.com>
2022-03-01 21:40:30 +0530

    style(change some styling): change some styling
    change some styling

--- src/components/app/Projects/Projects.tsx
+++ src/components/app/Projects/Projects.tsx
@@ -5,6 +5,7 @@ import ecommerce from "../../../assets/img/ecommerce.png";
 import alivio from "../../../assets/img/alivio.png";
 import musicApp from "../../../assets/img/music-app.png";
 import movieApp from "../../../assets/img/movie-app.png";
+import todoApp from "../../../assets/img/todo-app.png";
 import ScrollTrigger from "gsap/ScrollTrigger";
 import { gsap } from "gsap";
 import UnderlineHeading from "../../partials/UnderlineHeading/UnderlineHeading";
@@ -29,9 +30,9 @@ const useStyles = makeStyles({
 const projectData = [
   {
     id: 1,
-    name: "Ecommerce-App",
-    img: ecommerce,
+    name: "Ecommerce App",
     text: "A Ecommerce Web-App where user can create/delete a account, add item to cart/wishlist, ect. Web-App created with Next.Js, TS, MUI, Firebase.",
+    url: "https://ecommerce-app-six.vercel.app/",
     activeProjectBackground: {
       background: `linear-gradient(
       45deg,
@@ -47,9 +48,9 @@ const projectData = [
   },
   {
     id: 2,
-    name: "Music-App",
-    img: musicApp,
+    name: "Music App",
     text: "A Music Web-App where user have to login with there Spotify Account, but the user is only allowed to access the data if its email is enter in Spotify-Dashboard. Web-App created with Next.Js, TS, MUI.",
+    url: "https://music-app-liard.vercel.app/",
     activeProjectBackground: {
       background: `linear-gradient(
       45deg,
@@ -66,9 +67,9 @@ const projectData = [
   },
   {
     id: 3,
-    name: "Movie-App",
-    img: movieApp,
+    name: "Movie App",
     text: "A Movie Web-App where user can get information about Movies like :- description, cast, release date, rating, trailer, etc. Web-App created using React.Js & React-Redux.",
+    url: "https://react-movie-49d4a.web.app/",
     activeProjectBackground: {
       background: `linear-gradient(
       45deg,
@@ -86,8 +87,8 @@ const projectData = [
   {
     id: 4,
     name: "Landing Page",
-    img: alivio,
     text: "A Simple Landing Page create using Next.Js & MUI",
+    url: "https://alivio-landing-page-bay.vercel.app/",
     activeProjectBackground: {
       background: `linear-gradient(
       45deg,
@@ -97,6 +98,25 @@ const projectData = [
       transparent 50%
     ),
     url(${alivio})`,
+      backgroundSize: "contain",
+      backgroundRepeat: "no-repeat",
+      backgroundPositionX: "right",
+    },
+  },
+  {
+    id: 5,
+    name: "Todo App",
+    text: "A Todo App created using Next.Js, Redux, MUI, Firebase-SDK",
+    url: "https://todo-with-nextjs.vercel.app/",
+    activeProjectBackground: {
+      background: `linear-gradient(
+      45deg,
+      #3f3d56 0%,
+      #353346 49%,
+      #17151f 50%,
+      transparent 50%
+    ),
+    url(${todoApp})`,
       backgroundSize: "contain",
       backgroundRepeat: "no-repeat",
       backgroundPositionX: "right",
@@ -112,7 +132,7 @@ export default function Projects() {
   useEffect(() => {
     if (hover) return;
     const timeout = setTimeout(() => {
-      setActiveProject((prev) => (prev === 4 ? 1 : prev + 1));
+      setActiveProject((prev) => (prev === projectData.length ? 1 : prev + 1));
     }, 5000);
 
     return () => clearTimeout(timeout);
@@ -143,6 +163,8 @@ export default function Projects() {
                     </Typography>
 
                     <Button
+                      href={project.url}
+                      target="_blank"
                       variant="outlined"
                       className={classes.projectBtn}
                       color="secondary"
@@ -162,9 +184,15 @@ export default function Projects() {
               return (
                 <span
                   key={item.id}
-                  className={`${styles.projectBtn} ${
-                    item.id === activeProject ? styles.activeProjectBtn : ""
-                  }`}
+                  className={`${
+                    !hover ? styles.projectBtn : styles.disabledBtn
+                  } ${
+                    item.id === activeProject
+                      ? !hover
+                        ? styles.activeProjectBtn
+                        : styles.disabledActiveBtn
+                      : ""
+                  } `}
                   onClick={() => setActiveProject(item.id)}
                 />
               );
@@ -175,41 +203,3 @@ export default function Projects() {
     </section>
   );
 }
-
-/* {projectData.map((project, i) => {
-            return (
-              <div key={i} className={styles.project}>
-                <div
-                  id={`projectImage${i + 1}`}
-                  className={styles.projectImageContainer}
-                >
-                  <img
-                    src={project.img}
-                    alt="projectImg"
-                    className={styles.projectImg}
-                  />
-                </div>
-
-                <div
-                  id={`projectTextContainer${i + 1}`}
-                  className={styles.projectTextContainer}
-                >
-                  <Typography variant="h3">{project.name}</Typography>
-
-                  <Typography variant="body2" color="textPrimary">
-                    {project.text}
-                  </Typography>
-
-                  <Button
-                    variant="outlined"
-                    className={classes.projectBtn}
-                    color="secondary"
-                  >
-                    <Typography variant="subtitle1" color="secondary">
-                      View Project
-                    </Typography>
-                  </Button>
-                </div>
-              </div>
-            );
-          })} */
